9fa271019a8c469eaa8ca79abf0d91f2442e9704
Aydin Hassan <aydin@hotmail.co.uk>
2025-11-05 21:43:19 +0100

    public r2 domain

--- server/utils/r2.ts
+++ server/utils/r2.ts
@@ -1,7 +1,7 @@
 export function getPublicUrl(event: any, upload: { id: string; key: string }) {
   const base = process.dev
     ? '/api/image'
-    : event.context.cloudflare.env.R2_PUBLIC_BASE
+    : 'https://pub-fa37fe4a3c4f495997b4174ccf32a4e9.r2.dev'
 
   // use id for local (api route expects id) and key for prod (direct URL)
   return process.dev
